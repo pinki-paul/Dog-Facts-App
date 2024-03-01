09149e08025b402f502cf818bfefb0cfd5881471
Pinki Paul <pinkipaul371@gmail.com>
2024-03-01 14:49:37 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [dogFacts, setDogFacts] = useState(null);
+
+  const fetchDogFacts = () => {
+    Axios.get("https://dog-api.kinduff.com/api/facts").then((res) => {
+      setDogFacts(res.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchDogFacts();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='dogs'>
+      <h1>Dog Facts App</h1>
+      <div className='img'>
+      <img src='https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg' alt='dog' />
+      </div>
+    
+      </div>
+     
+      <div className='container'>
+        <button className='btn' onClick={fetchDogFacts}>touch paw</button>
+        <p>Facts: <span> {dogFacts?.facts}</span></p>
+      </div>
     </div>
   );
 }
